d346407648c53deffaf4cbf22d7f414d0a20c41f
reggie-db <reggie.pierce@databricks.com>
2025-09-16 10:26:41 -0400

    Update logs.py

--- reggie_tools/logs.py
+++ reggie_tools/logs.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from typing import Optional
+from reggie_tools import runtimes
 
 
 def logger(name: Optional[str] = None) -> logging.Logger:
@@ -16,9 +17,10 @@ def logger(name: Optional[str] = None) -> logging.Logger:
         log.propagate = False
         log.setLevel(logging.DEBUG)
         handler_levels = {sys.stdout: logging.INFO, sys.stderr: logging.WARNING}
-        print(
-            f"logger created - name:{name} stdout_level:{handler_levels[sys.stdout]} stderr_level:{handler_levels[sys.stderr]}"
-        )
+        if not runtimes.version():
+            print(
+                f"logger created - name:{name} stdout_level:{handler_levels[sys.stdout]} stderr_level:{handler_levels[sys.stderr]}"
+            )
         fmt = logging.Formatter("%(asctime)s %(levelname)s [%(name)s] %(message)s")
         for stream, level in handler_levels.items():
             handler = logging.StreamHandler(stream)
