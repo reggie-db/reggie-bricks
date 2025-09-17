48b9c2fc0bb558ccfb90d8907ad2cc04856fc8a3
reggie-db <reggie.pierce@databricks.com>
2025-09-17 15:23:04 -0400

    Create jsons.py

---
+++ reggie_tools/jsons.py
@@ -0,0 +1,17 @@
+import datetime
+import json
+from dataclasses import is_dataclass, asdict
+from typing import Any
+
+
+def dumps(obj: Any) -> Any:
+    return json.dumps(obj, cls=DataclassJSONEncoder)
+
+
+class DataclassJSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, datetime):
+            return o.isoformat()
+        if is_dataclass(o):
+            return asdict(o)
+        return super().default(o)
